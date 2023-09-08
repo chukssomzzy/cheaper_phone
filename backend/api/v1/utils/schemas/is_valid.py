f1cac264ed2091905d55675ab7946ecdd837c481
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2023-09-08 08:08:11 -0700

    schema

---
+++ backend/api/v1/utils/schemas/is_valid.py
@@ -0,0 +1,27 @@
+#!/venv/bin/python3
+
+"""Decorator to validate a schema with a request object"""
+from flask import request
+
+from functools import wraps
+from jsonschema import Draft202012Validator
+from api.v1.utils.schema.resolver import register
+
+
+def isvalid(uri_ref):
+    """checks if the request object of a particular
+    request has the correct decorator
+    """
+    def decorator_isvalid(f)
+      @wraps(f)
+       def decorated_function(*args, **kwargs):
+            body = request.get_json()
+            validator = Draft202012Validator(
+                {"type": "object",
+                 "additionalProperties": {"$ref": f"{uri_ref}"}
+                 },
+                registry=register
+            )
+            validator.validate(body)
+            return f(*args, **kwargs)
+        return decorated_function
--- backend/api/v1/utils/schemas/resolver.py
+++ backend/api/v1/utils/schemas/resolver.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env venv/bin/python3
+"""Referencing and schema from file system"""
+
 from pathlib import Path
 import json
 
-from refrencing import Registry, Res
+from refrencing import Registry, Resource
+from refrencing.exception import NoSuchResource
+
+SCHEMAS = Path(".")
+
+
+def retrieve_from_dir(uri: str):
+    """Retrieve a json schema from filesystem"""
+    if not uri.startwith("http://localhost/"):
+        raise NoSuchResource(ref=uri)
+    path = SCHEMAS / Path(uri.removeprefix("http://localhost/"))
+    content = json.load(path)
+    Resource.from_content(content)
+
+
+registry = Registry(retrieve=retrieve_from_dir)
