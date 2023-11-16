886bccbcf46fd23dd0ac5a94b0ca96fe85bfd7aa
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2023-11-16 16:03:26 +0000

    wrong foreign key id type in user_cart_products

--- backend/models/user_cart_products.py
+++ backend/models/user_cart_products.py
@@ -11,7 +11,7 @@ class UserCartProduct(BaseModel, Base):
     """Defines column for foreignKey between the product and user_cart"""
     __tablename__ = "user_cart_products"
     id = None
-    user_cart_id = Column(Integer, ForeignKey(
+    user_cart_id = Column(String(60), ForeignKey(
         "user_cart.id"), primary_key=True)
     product_id = Column(String(60), ForeignKey(
         "products.id"), primary_key=True)
